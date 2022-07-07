4bcf2464d034596cd8087da62fff6cc97d4fdfdc
piero0090 <pierocuzcano@hotmail.com>
2022-07-07 11:49:56 -0500

    Registro pantalla I

--- app/src/main/java/com/example/programovilfinal/LoginActivity.kt
+++ app/src/main/java/com/example/programovilfinal/LoginActivity.kt
@@ -4,6 +4,7 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
+import android.widget.TextView
 
 class LoginActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -11,9 +12,15 @@ class LoginActivity : AppCompatActivity() {
         setContentView(R.layout.activity_login)
 
         val btniniciosesion = findViewById<Button>(R.id.btnInicioSesion)
+        val txtvRegistro = findViewById<TextView>(R.id.tvRegistrate)
 
         btniniciosesion.setOnClickListener {
             val intent = Intent(this,MainActivity::class.java)
+            startActivity(intent)
+        }
+
+        txtvRegistro.setOnClickListener {
+            val intent = Intent(this,RegistroActivity::class.java)
             startActivity(intent)
         }
     }
