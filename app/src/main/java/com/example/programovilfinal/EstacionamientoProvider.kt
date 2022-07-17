afb6bb257c57838c996e90a532c7ae098fe78656
Sorio97 <39248528+Sorio97@users.noreply.github.com>
2022-07-17 18:23:34 -0500

    Recyclerview

---
+++ app/src/main/java/com/example/programovilfinal/EstacionamientoProvider.kt
@@ -0,0 +1,33 @@
+package com.example.programovilfinal
+
+class EstacionamientoProvider {
+    companion object {
+        val estacionamientoList = listOf<Estacionamiento>(
+        Estacionamiento(
+        "1",
+        "V"
+        ),
+        Estacionamiento(
+        "2",
+        "V"
+        ),
+        Estacionamiento(
+        "3",
+        "V"
+        ),
+        Estacionamiento(
+        "1",
+        "R"
+        ),
+        Estacionamiento(
+        "2",
+        "R"
+        ),
+        Estacionamiento(
+        "3",
+        "R"
+        )
+
+        )
+    }
+}
