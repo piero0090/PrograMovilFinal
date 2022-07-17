17f54fa45cf34d9ea3df8f2f94674ea873bae5c5
clau3355 <73697504+clau3355@users.noreply.github.com>
2022-07-17 01:53:25 -0500

    cambios QR

--- app/src/main/java/com/example/programovilfinal/ui/Fragments/RegistroposFragment.kt
+++ app/src/main/java/com/example/programovilfinal/ui/Fragments/RegistroposFragment.kt
@@ -1,11 +1,19 @@
 package com.example.programovilfinal.ui.Fragments
 
+import android.content.Intent
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
+import android.widget.TextView
+import android.widget.Toast
 import com.example.programovilfinal.R
+import com.example.programovilfinal.databinding.ActivityMainBinding
+import com.google.zxing.integration.android.IntentIntegrator
+import org.json.JSONObject
+import org.w3c.dom.Text
 
 // TODO: Rename parameter arguments, choose names that match
 // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
@@ -18,17 +26,12 @@ private const val ARG_PARAM2 = "param2"
  * create an instance of this fragment.
  */
 class RegistroposFragment : Fragment() {
-    // TODO: Rename and change types of parameters
-    private var param1: String? = null
-    private var param2: String? = null
 
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        arguments?.let {
-            param1 = it.getString(ARG_PARAM1)
-            param2 = it.getString(ARG_PARAM2)
-        }
-    }
+    //private lateinit var binding: ActivityMainBinding
+    private lateinit var btnScan : Button
+    private lateinit var txtRegistroPos : TextView
+    private lateinit var qrScanIntegrator: IntentIntegrator
+
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -38,6 +41,32 @@ class RegistroposFragment : Fragment() {
         return inflater.inflate(R.layout.fragment_registropos, container, false)
     }
 
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        btnScan = view.findViewById(R.id.btnScanner)
+        btnScan.setOnClickListener{InnitScanner()}
+        txtRegistroPos = view.findViewById(R.id.txtID_Posicion)
+    }
+
+    private fun InnitScanner(){
+        qrScanIntegrator = IntentIntegrator.forSupportFragment(this)
+        qrScanIntegrator.initiateScan()
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        val result = IntentIntegrator.parseActivityResult(requestCode,resultCode,data)
+        if(result!=null){
+            if(result.contents == null){
+                Toast.makeText(activity, "pruebita", Toast.LENGTH_LONG).show()
+            }else{
+                Toast.makeText(activity, "POSICIÓN REGISTRADA", Toast.LENGTH_LONG).show()
+                txtRegistroPos.text = result.contents
+            }
+        }else{
+            super.onActivityResult(requestCode, resultCode, data)
+        }
+    }
+    /*
     companion object {
         /**
          * Use this factory method to create a new instance of
@@ -56,5 +85,5 @@ class RegistroposFragment : Fragment() {
                     putString(ARG_PARAM2, param2)
                 }
             }
-    }
+    }*/
 }
