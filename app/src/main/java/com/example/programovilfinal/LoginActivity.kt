3a8ef1beafa5e41b011fbccb3848441f95b5abb5
piero0090 <pierocuzcano@hotmail.com>
2022-07-17 17:17:47 -0500

    Login Insert creados

--- app/src/main/java/com/example/programovilfinal/LoginActivity.kt
+++ app/src/main/java/com/example/programovilfinal/LoginActivity.kt
@@ -3,20 +3,39 @@ package com.example.programovilfinal
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import android.widget.Button
+import android.widget.EditText
 import android.widget.TextView
+import android.widget.Toast
 
 class LoginActivity : AppCompatActivity() {
+
+    private lateinit var edtUser:EditText
+    private lateinit var edtPass:EditText
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
 
         val btniniciosesion = findViewById<Button>(R.id.btnInicioSesion)
         val txtvRegistro = findViewById<TextView>(R.id.tvRegistrate)
+        edtUser= findViewById(R.id.edtUserCod)
+        edtPass= findViewById(R.id.edtPass)
 
         btniniciosesion.setOnClickListener {
-            val intent = Intent(this,MainActivity::class.java)
-            startActivity(intent)
+            GestUsers.getInstance().LoginUsers(edtUser.text.toString().toInt(),edtPass.text.toString()){
+                if (it==null){
+                    val toast= Toast.makeText(applicationContext,"CONTRASEÑA/PASSWORD INCORRECTOS",Toast.LENGTH_SHORT)
+                    toast.show()
+                    //Log.i("GAA","NOOOOOOOOOOOOOOOO")
+                }else{
+                    val intent = Intent(this,MainActivity::class.java)
+                    startActivity(intent)
+                    //Log.i("asa","SUCCCESSSSS")
+                }
+            }
+
         }
 
         txtvRegistro.setOnClickListener {
---
+++ app/src/main/java/com/example/programovilfinal/Usuarios.kt
@@ -0,0 +1,8 @@
+package com.example.programovilfinal
+
+data class Usuarios(
+    val id: String,
+    val usercod: Number,
+    val contrass: String,
+)
+
