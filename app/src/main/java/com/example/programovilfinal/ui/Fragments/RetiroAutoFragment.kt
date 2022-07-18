a37b1ea8d39b555d5c8ee68314888080ea268254
clau3355 <73697504+clau3355@users.noreply.github.com>
2022-07-17 21:47:10 -0500

    cambios retiro

--- app/src/main/java/com/example/programovilfinal/ui/Fragments/RetiroAutoFragment.kt
+++ app/src/main/java/com/example/programovilfinal/ui/Fragments/RetiroAutoFragment.kt
@@ -5,6 +5,7 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
 import com.example.programovilfinal.R
 
 // TODO: Rename parameter arguments, choose names that match
@@ -18,17 +19,7 @@ private const val ARG_PARAM2 = "param2"
  * create an instance of this fragment.
  */
 class RetiroAutoFragment : Fragment() {
-    // TODO: Rename and change types of parameters
-    private var param1: String? = null
-    private var param2: String? = null
-
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        arguments?.let {
-            param1 = it.getString(ARG_PARAM1)
-            param2 = it.getString(ARG_PARAM2)
-        }
-    }
+    private lateinit var btnRetiro : Button
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -38,23 +29,15 @@ class RetiroAutoFragment : Fragment() {
         return inflater.inflate(R.layout.fragment_retiro_auto, container, false)
     }
 
-    companion object {
-        /**
-         * Use this factory method to create a new instance of
-         * this fragment using the provided parameters.
-         *
-         * @param param1 Parameter 1.
-         * @param param2 Parameter 2.
-         * @return A new instance of fragment RetiroAutoFragment.
-         */
-        // TODO: Rename and change types and number of parameters
-        @JvmStatic
-        fun newInstance(param1: String, param2: String) =
-            RetiroAutoFragment().apply {
-                arguments = Bundle().apply {
-                    putString(ARG_PARAM1, param1)
-                    putString(ARG_PARAM2, param2)
-                }
-            }
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        btnRetiro = view.findViewById(R.id.btnDgRetiro)
+        btnRetiro.setOnClickListener{MostrarDialogo()}
     }
+
+    private fun MostrarDialogo(){
+        DialogRetiroFragmento().show(
+            childFragmentManager, DialogRetiroFragmento.TAG)
+    }
+
 }
