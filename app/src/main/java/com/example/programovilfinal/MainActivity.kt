e33525f4e43a73233dc2cfa1e12c408ed1045065
piero0090 <pierocuzcano@hotmail.com>
2022-07-17 23:56:43 -0500

    UserCode in Menu

--- app/src/main/java/com/example/programovilfinal/MainActivity.kt
+++ app/src/main/java/com/example/programovilfinal/MainActivity.kt
@@ -5,7 +5,9 @@ import android.content.ClipData
 import android.os.Bundle
 import android.view.Menu
 import android.view.MenuItem
+import android.view.View
 import android.widget.Button
+import android.widget.TextView
 import android.widget.Toast
 import com.google.android.material.snackbar.Snackbar
 import com.google.android.material.navigation.NavigationView
@@ -26,20 +28,20 @@ class MainActivity : AppCompatActivity() {
     private lateinit var appBarConfiguration: AppBarConfiguration
     private lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
+
         super.onCreate(savedInstanceState)
         EstacionamientoProvider.estacionamientoList
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
         setSupportActionBar(binding.appBarMain.toolbar)
 
-        /*
-        binding.appBarMain.fab.setOnClickListener { view ->
-            Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                .setAction("Action", null).show()
-        }*/
         val drawerLayout: DrawerLayout = binding.drawerLayout
         val navView: NavigationView = binding.navView
         val navController = findNavController(R.id.nav_host_fragment_content_main)
+        //Insertar datos en el Menu Header
+        val header: View =navView.getHeaderView(0)
+        val txtnombre : TextView = header.findViewById(R.id.txtNav)
+        txtnombre.setText(intent.getStringExtra("Usercod"))
 
         appBarConfiguration = AppBarConfiguration(
             setOf(
@@ -48,16 +50,11 @@ class MainActivity : AppCompatActivity() {
         )
         setupActionBarWithNavController(navController, appBarConfiguration)
         navView.setupWithNavController(navController)
-
     }
 
     override fun onSupportNavigateUp(): Boolean {
         val navController = findNavController(R.id.nav_host_fragment_content_main)
         return navController.navigateUp(appBarConfiguration) || super.onSupportNavigateUp()
     }
-    fun initRecyclerView(){
-       // val recyclerView = findViewById<RecyclerView>(R.id.recyclerEstacionamiento)
-        //recyclerView.layoutManager = LinearLayoutManager (this)
-        //recyclerView.adapter = EstacionamientoAdapter(EstacionamientoProvider.estacionamientoList)
-    }
+
 }
