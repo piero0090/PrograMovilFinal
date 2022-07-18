36550661763086f540a7693a66d3c3285146e256
piero0090 <pierocuzcano@hotmail.com>
2022-07-18 01:03:04 -0500

    Avatares Menu

--- app/src/main/java/com/example/programovilfinal/MainActivity.kt
+++ app/src/main/java/com/example/programovilfinal/MainActivity.kt
@@ -3,10 +3,12 @@ package com.example.programovilfinal
 import android.app.Dialog
 import android.content.ClipData
 import android.os.Bundle
+import android.util.Log
 import android.view.Menu
 import android.view.MenuItem
 import android.view.View
 import android.widget.Button
+import android.widget.ImageView
 import android.widget.TextView
 import android.widget.Toast
 import com.google.android.material.snackbar.Snackbar
@@ -22,8 +24,12 @@ import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import com.example.programovilfinal.adapter.EstacionamientoAdapter
 import com.example.programovilfinal.databinding.ActivityMainBinding
+import kotlin.random.Random
 
 class MainActivity : AppCompatActivity() {
+
+    var avatars= arrayOf(R.drawable.img01, R.drawable.img02, R.drawable.img03, R.drawable.img04,
+        R.drawable.img05, R.drawable.img06)
 
     private lateinit var appBarConfiguration: AppBarConfiguration
     private lateinit var binding: ActivityMainBinding
@@ -38,14 +44,18 @@ class MainActivity : AppCompatActivity() {
         val drawerLayout: DrawerLayout = binding.drawerLayout
         val navView: NavigationView = binding.navView
         val navController = findNavController(R.id.nav_host_fragment_content_main)
+
         //Insertar datos en el Menu Header
         val header: View =navView.getHeaderView(0)
         val txtnombre : TextView = header.findViewById(R.id.txtNav)
+        val imgAvatar : ImageView = header.findViewById(R.id.imgMenu)
+        imgAvatar.setImageResource(avatars[Random.nextInt(0,6)])
         txtnombre.setText(intent.getStringExtra("Usercod"))
 
         appBarConfiguration = AppBarConfiguration(
             setOf(
-                R.id.nav_editar, R.id.nav_retirar, R.id.nav_buscar,R.id.nav_registrar, R.id.nav_info, R.id.nav_emergencia
+                R.id.nav_editar, R.id.nav_retirar, R.id.nav_buscar,R.id.nav_registrar,
+                R.id.nav_info, R.id.nav_emergencia
             ), drawerLayout
         )
         setupActionBarWithNavController(navController, appBarConfiguration)
